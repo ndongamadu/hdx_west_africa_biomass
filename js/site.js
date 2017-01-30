a29cec8e2307a33e29ee98a3638ca5b28a24cead
Amadu <ndongamadou@gmail.com>
2017-01-30 22:09:45 +0000

    various edits and new files

--- js/site.js
+++ js/site.js
@@ -1,23 +1,59 @@
 d3.json("data/biomass.json", function(biomasse_data) {
 
+
+  biomasse_data.forEach(function(d){
+      d.ID = +d.ID ;
+      d.MEAN = +d.MEAN ;
+  });
+
   var biomasseChart = dc.barChart('#biomasseGraphe');
 
   var ndx = crossfilter(biomasse_data);
   var dimension98 = ndx.dimension(function(d){return d.ID});
-  var dimensionGroup = dimension98.group();
+  var dimensionGroup = dimension98.group().reduceSum(function(d){
+    return d.MEAN ;
+  });
 
-  biomasseChart.width(650)
+  biomasseChart.width(450)
               .height(300)
-              .x(d3.scale.linear())
-            //  .yAxisLabel("This is the Y Axis!")
+              .x(d3.scale.linear().domain([1, 15]))
               .group(dimensionGroup)
               .dimension(dimension98)
             //  .renderHorizontalGridLines(true)
               .elasticX(true)
-              //.colors(["#a60000","#ff0000", "#ff4040","#ff7373","#67e667","#39e639","#00cc00"])
-              .label(function(d){
-                return d.NAME;
-              });
+              //.xAxis().ticks(4)
+              // .label(function(d){
+              //   return d.NAME;
+              // })
+              .render();
+});
 
-dc.renderAll();
+d3.csv('data/test.csv', function(csvdata) {
+
+    // csvdata.forEach(function(d){
+    //     d.annee = d.annee ;
+    //     d.biomasse = +d.biomasse ;
+    // });
+
+
+    var csvChart = dc.barChart('#grapheAnomalie') ;
+
+    var cd = crossfilter(csvdata);
+    var csvDim = cd.dimension(function(d){ return d.annee});
+    var csvGroup = csvDim.group().reduceSum(function(d){
+      return d.biomasse;
+    });
+
+
+    csvChart.width(450)
+            .height(300)
+            .dimension(csvDim)
+            .group(csvGroup)
+            .x(d3.scale.linear().domain([1998, 2007]))
+            .elasticY(true)
+            .yAxisLabel("Axe des Y!")
+            //.colors(["#a60000","#ff0000", "#ff4040","#ff7373","#67e667","#39e639","#00cc00"])
+	          .xAxis().tickFormat();
+
+    csvChart.render();
 });
